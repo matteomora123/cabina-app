8ebae99d42ad354f950883a650181d548db568b3
repfdelpiano <ext.repfdelpiano@cdp.it>
2025-08-04 18:22:46 +0200

    Modificato script start con path relativi

--- start_all.bat
+++ start_all.bat
@@ -1,12 +1,12 @@
 @echo off
 echo Avvio Backend (FastAPI)...
-start cmd /k "cd /d C:\Users\matte\PycharmProjects\cabina-app\backend && uvicorn main:app --reload"
+start cmd /k "cd /d backend && uvicorn main:app --reload"
 
 echo Avvio Microservizio AI (porta 9000)...
-start cmd /k "cd /d C:\Users\matte\PycharmProjects\cabina-app\backend && uvicorn ai_microservice.ai_api:app --port 9000 --reload"
+start cmd /k "cd /d backend && uvicorn ai_microservice.ai_api:app --port 9000 --reload"
 
 echo Avvio Frontend (React)...
-start cmd /k "cd /d C:\Users\matte\PycharmProjects\cabina-app\frontend && npm start"
+start cmd /k "cd /d frontend && npm start"
 
 echo Tutto avviato.
 pause
